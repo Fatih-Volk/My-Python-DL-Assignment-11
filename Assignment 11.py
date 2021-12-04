c33af0877cc8effe2377b4a2655fcc140b0f29a1
Fatih <39358150+Fatih-Volk@users.noreply.github.com>
2021-12-03 23:49:56 -0600

    Homework Push

---
+++ Assignment 11.py
@@ -0,0 +1,253 @@
+#### This is Python 2 CIS 2532 assignment 11, by Mig. Mir.
+#### Base code taken from GitHub user @ josephlee94, with guidance from guide @ https://medium.com/intuitive-deep-learning/build-your-first-convolutional-neural-network-to-recognize-images-84b9c78fe0ce
+#### Thank you!
+
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context      #### code to resolve certificate error in part 2
+
+def part1():
+      import pandas as pd
+
+      df = pd.read_csv('housepricedata.csv')
+
+      df
+
+      dataset = df.values
+
+      dataset
+
+      X = dataset[:,0:10]
+      Y = dataset[:,10]
+
+      from sklearn import preprocessing
+
+      min_max_scaler = preprocessing.MinMaxScaler()
+      X_scale = min_max_scaler.fit_transform(X)
+
+      X_scale
+
+      from sklearn.model_selection import train_test_split
+      X_train, X_val_and_test, Y_train, Y_val_and_test = train_test_split(X_scale, Y, test_size=0.3)
+      X_val, X_test, Y_val, Y_test = train_test_split(X_val_and_test, Y_val_and_test, test_size=0.5)
+      print(X_train.shape, X_val.shape, X_test.shape, Y_train.shape, Y_val.shape, Y_test.shape)
+
+      from keras.models import Sequential
+      from keras.layers import Dense
+
+      model = Sequential([
+      Dense(32, activation='relu', input_shape=(10,)),
+      Dense(32, activation='relu'),
+      Dense(1, activation='sigmoid'),
+      ])
+
+      model.compile(optimizer='sgd',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+      hist = model.fit(X_train, Y_train,
+            batch_size=32, epochs=100,
+            validation_data=(X_val, Y_val))
+
+      model.evaluate(X_test, Y_test)[1]
+
+      import matplotlib.pyplot as plt
+
+      plt.plot(hist.history['loss'])
+      plt.plot(hist.history['val_loss'])
+      plt.title('Model loss')
+      plt.ylabel('Loss')
+      plt.xlabel('Epoch')
+      plt.legend(['Train', 'Val'], loc='upper right')
+      plt.show()
+
+      plt.plot(hist.history['accuracy'])
+      plt.plot(hist.history['val_accuracy'])
+      plt.title('Model accuracy')
+      plt.ylabel('Accuracy')
+      plt.xlabel('Epoch')
+      plt.legend(['Train', 'Val'], loc='lower right')
+      plt.show()
+
+      model_2 = Sequential([
+      Dense(1000, activation='relu', input_shape=(10,)),
+      Dense(1000, activation='relu'),
+      Dense(1000, activation='relu'),
+      Dense(1000, activation='relu'),
+      Dense(1, activation='sigmoid'),
+      ])
+      model_2.compile(optimizer='adam',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+      hist_2 = model_2.fit(X_train, Y_train,
+            batch_size=32, epochs=100,
+            validation_data=(X_val, Y_val))
+
+      plt.plot(hist_2.history['loss'])
+      plt.plot(hist_2.history['val_loss'])
+      plt.title('Model loss')
+      plt.ylabel('Loss')
+      plt.xlabel('Epoch')
+      plt.legend(['Train', 'Val'], loc='upper right')
+      plt.show()
+
+      plt.plot(hist_2.history['accuracy'])
+      plt.plot(hist_2.history['val_accuracy'])
+      plt.title('Model accuracy')
+      plt.ylabel('Accuracy')
+      plt.xlabel('Epoch')
+      plt.legend(['Train', 'Val'], loc='lower right')
+      plt.show()
+
+      from keras.layers import Dropout
+      from keras import regularizers
+
+      model_3 = Sequential([
+      Dense(1000, activation='relu', kernel_regularizer=regularizers.l2(0.01), input_shape=(10,)),
+      Dropout(0.3),
+      Dense(1000, activation='relu', kernel_regularizer=regularizers.l2(0.01)),
+      Dropout(0.3),
+      Dense(1000, activation='relu', kernel_regularizer=regularizers.l2(0.01)),
+      Dropout(0.3),
+      Dense(1000, activation='relu', kernel_regularizer=regularizers.l2(0.01)),
+      Dropout(0.3),
+      Dense(1, activation='sigmoid', kernel_regularizer=regularizers.l2(0.01)),
+      ])
+
+      model_3.compile(optimizer='adam',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+      hist_3 = model_3.fit(X_train, Y_train,
+            batch_size=32, epochs=100,
+            validation_data=(X_val, Y_val))
+
+      plt.plot(hist_3.history['loss'])
+      plt.plot(hist_3.history['val_loss'])
+      plt.title('Model loss')
+      plt.ylabel('Loss')
+      plt.xlabel('Epoch')
+      plt.legend(['Train', 'Val'], loc='upper right')
+      plt.ylim(top=1.2, bottom=0)
+      plt.show()
+
+      plt.plot(hist_3.history['accuracy'])
+      plt.plot(hist_3.history['val_accuracy'])
+      plt.title('Model accuracy')
+      plt.ylabel('Accuracy')
+      plt.xlabel('Epoch')
+      plt.legend(['Train', 'Val'], loc='lower right')
+      plt.show()
+
+def part2():
+      from keras.datasets import cifar10
+      (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+      print('x_train shape:', x_train.shape)
+
+      print('y_train shape:', y_train.shape)
+
+      print(x_train[0])
+
+      #%matplotlib inline           #### despite having 
+      import matplotlib.pyplot as plt
+
+
+      img = plt.imshow(x_train[0])
+      print('The label is:', y_train[0])
+      img = plt.imshow(x_train[1])
+      print('The label is:', y_train[1])
+
+      import keras
+      from tensorflow.keras.utils import to_categorical     #### changed from original code, as it was creating errors
+      y_train_one_hot = to_categorical(y_train, 10)
+      y_test_one_hot = to_categorical(y_test, 10)
+
+      print('The one hot label is:', y_train_one_hot[1])
+
+      x_train = x_train.astype('float32')
+      x_test = x_test.astype('float32')
+      x_train = x_train / 255
+      x_test = x_test / 255
+
+      x_train[0]
+
+      from keras.models import Sequential
+      from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+
+      model = Sequential()
+
+      model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32,32,3)))
+      model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+
+      model.add(MaxPooling2D(pool_size=(2, 2)))
+      model.add(Dropout(0.25))
+
+      model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+      model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+      model.add(MaxPooling2D(pool_size=(2, 2)))
+      model.add(Dropout(0.25))
+
+      model.add(Flatten())
+
+      model.add(Dense(512, activation='relu'))
+
+      model.add(Dropout(0.5))
+
+      model.add(Dense(10, activation='softmax'))
+
+      model.summary()
+
+      model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+      hist = model.fit(x_train, y_train_one_hot, 
+            batch_size=32, epochs=20, 
+            validation_split=0.2)
+
+      plt.plot(hist.history['loss'])
+      plt.plot(hist.history['val_loss'])
+      plt.title('Model loss')
+      plt.ylabel('Loss')
+      plt.xlabel('Epoch')
+      plt.legend(['Train', 'Val'], loc='upper right')
+      plt.show()
+
+      plt.plot(hist.history['accuracy'])
+      plt.plot(hist.history['val_accuracy'])
+      plt.title('Model accuracy')
+      plt.ylabel('Accuracy')
+      plt.xlabel('Epoch')
+      plt.legend(['Train', 'Val'], loc='lower right')
+      plt.show()
+
+      model.evaluate(x_test, y_test_one_hot)[1]
+
+      model.save('my_cifar10_model.h5')
+
+
+      ##### testing with CAT image(s)
+      my_image = plt.imread("cat.jpg")
+      from skimage.transform import resize
+      my_image_resized = resize(my_image, (32,32,3))
+
+      img = plt.imshow(my_image_resized)
+
+      import numpy as np
+      probabilities = model.predict(np.array( [my_image_resized,] ))
+
+      probabilities
+
+      number_to_class = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+      index = np.argsort(probabilities[0,:])
+      print("Most likely class:", number_to_class[index[9]], "-- Probability:", probabilities[0,index[9]])
+      print("Second most likely class:", number_to_class[index[8]], "-- Probability:", probabilities[0,index[8]])
+      print("Third most likely class:", number_to_class[index[7]], "-- Probability:", probabilities[0,index[7]])
+      print("Fourth most likely class:", number_to_class[index[6]], "-- Probability:", probabilities[0,index[6]])
+      print("Fifth most likely class:", number_to_class[index[5]], "-- Probability:", probabilities[0,index[5]])
+
+
+      #### test comment
+
+#part1()
+part2()
